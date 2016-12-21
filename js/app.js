7e835682df6872c5f7bd3f2ac2db859f42446bef
Ryca77 <ryan.carter77@gmail.com>
2016-12-21 12:01:54 +0000

    added code links and made styling changes

--- js/app.js
+++ js/app.js
@@ -3,25 +3,25 @@ $(document).ready(function() {
 $(".name").click(function() {
 	$('html,body').animate( {
 		scrollTop: $("#background").offset().top
-    } ,'1000');
+    } , 1500);
 });
 
 $(".nav1, .menu1").click(function() {
 	$('html,body').animate( {
 		scrollTop: $(".projects").offset().top - 70
-    } ,'1000');
+    } , 1500);
 });
 
 $(".nav2, .menu2").click(function() {
 	$('html,body').animate( {
 		scrollTop: $(".about-me").offset().top - 70
-    } ,'1000');
+    } , 1500);
 });
 
 $(".nav3, .menu3").click(function() {
 	$('html,body').animate( {
 		scrollTop: $(".contact").offset().top - 70
-    } ,'1000');
+    } , 1500);
 });
 
 $('.menu-icon, .close-icon').on('click', function() {
